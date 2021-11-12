9d636546b84694c97cdebf762ea7e3ae2699db9e
Andrea Kalaziova <akalaziova@hotmail.com>
2021-11-12 05:37:52 +0100

    oprava zobrazFoto()

--- main.js
+++ main.js
@@ -51,30 +51,30 @@ let obrazky = [
     'sova.jpg',
     'zajic.jpg'
 ];
-let fotka = document.querySelector("#foto")
-let popisek = document.querySelector("#pocitadlo")
-let sipka = document.querySelector(".sipka")
+let fotka = document.querySelector("#foto");
+let popisek = document.querySelector("#pocitadlo");
+let sipka = document.querySelector(".sipka");
+let p = 0;
 fotka.addEventListener("click", zobrazFoto)
 fotka.src = "obrazky/kocka.jpg"
 popisek.textContent = "kocka.jpg - 0 / " + obrazky.length
 
+sipka.addEventListener("click", posunFoto)
+
 function zobrazFoto() {
+    if (p < (obrazky.length - 1)) {
 
-    for (let i = 0; i < obrazky.length; i++) {
-        fotka.src = "obrazky/" + obrazky[i]
-
-        popisek.textContent = obrazky[i] + " - " + i + " / " + obrazky.length
-
+        fotka.src = "obrazky/" + obrazky[p + 1]
+        popisek.textContent = obrazky[p + 1] + " - " + (p + 1) + " / " + obrazky.length
+        p++
     }
-
 }
 
-// if (sipka === )
+
+
+
 
 /*
-funkce posun doprava
-funkce posun doleva
-
 Tvoříš galerii obrázků. Seznam obrázků máš uložený v poli obrazky[].
 Z celé galerie je vidět vždy jen jeden obrázek.
 Na stránce jsou tlačítka "Předchozí" a "Další"- při stisknutí tlačítka zobraz předchozí/následující obrázek (nahraď zdroj "src" obrázku jménem nového obrázku).
