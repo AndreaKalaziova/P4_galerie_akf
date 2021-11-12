05f6dab32a41bf65bf759d17b27251cc62466559
Andrea Kalaziova <akalaziova@hotmail.com>
2021-11-12 21:27:11 +0100

    Bonus_nahled_hotovo

--- main.js
+++ main.js
@@ -64,7 +64,8 @@ sipkaL.addEventListener("click", fotoZpet)
 
 function zobrazFoto() {
     fotka.src = "obrazky/" + obrazky[p];
-    popisek.textContent = obrazky[p] + " - " + p + " / " + obrazky.length
+    popisek.textContent = obrazky[p] + " - " + p + " / " + obrazky.length;
+    nahled();
 }
 
 function fotoVpred() {
@@ -83,10 +84,17 @@ function fotoZpet() {
     zobrazFoto();
 }
 
-/*
-Bonusový úkol:
-==============
-
-Tlačítka předchozí/následující nahraď malým náhledem dalšího/
-předcházejícího obrázku. Všechny tři obrázky (velký aktuální a malý předchozí/následující se samozřejmě budou měnit adekvátně tomu, jak procházíš galerií.
-*/
+function nahled() {
+    sipkaP.src = "obrazky/" + obrazky[p + 1];
+    sipkaL.src = "obrazky/" + obrazky[p - 1];
+    sipkaP.style.width = "150px"
+    sipkaP.style.borderRadius = "0%"
+    sipkaL.style.width = "150px"
+    sipkaL.style.borderRadius = "0%"
+    if (p == obrazky.length - 1) {
+        sipkaP.src = "obrazky/" + obrazky[0];;
+    }
+    if (p == 0) {
+        sipkaL.src = "obrazky/" + obrazky[obrazky.length - 1];
+    }
+}
